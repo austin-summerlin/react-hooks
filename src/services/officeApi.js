3aa5dd37725a8aab1a7d5a6505d3583881d79f4f
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-02 15:19:26 -0700

    fetchQuotes

--- src/services/officeApi.js
+++ src/services/officeApi.js
@@ -1,2 +1,12 @@
 //https://officeapi.dev/api/quotes/random
 
+export const fetchQuotes = async () => {
+  const res = await fetch('https://officeapi.dev/api/quotes/random');
+  const json = await res.json();
+
+  return json.map((quote) => ({
+    content: quote.content,
+    character: quote.character,
+  }));
+};
+
